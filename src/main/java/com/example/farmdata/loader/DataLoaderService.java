d9fc86927f088758a4349a7dfd1965f156472361
Heidi Hulkko <hulkhei@gmail.com>
2022-01-16 17:51:17 +0200

    Make data loading faster by removing extra repository calls

--- src/main/java/com/example/farmdata/loader/DataLoaderService.java
+++ src/main/java/com/example/farmdata/loader/DataLoaderService.java
@@ -1,5 +1,6 @@
 package com.example.farmdata.loader;
 
+import com.example.farmdata.data.SensorType;
 import com.example.farmdata.database.FarmRepository;
 import com.example.farmdata.database.MeasurementRepository;
 import com.example.farmdata.database.SensorTypeEntity;
@@ -11,7 +12,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Service
@@ -27,24 +31,34 @@ public class DataLoaderService {
 
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void saveAll(List<FarmDataItem> data) {
+        var sensorTypes = getSavedSensorTypes();
         var farmToSave = FarmDataMapper.mapToFarm(data.get(0));
         log.info("Starting to save measurement data from {}", farmToSave.getName());
         var farm = farmRepository.save(farmToSave);
         data.forEach(item -> {
-            var sensorType = findSensorType(item);
+            var sensorType = sensorTypes.get(item.getSensorType());
             var measurement = FarmDataMapper.mapToMeasurement(item);
             measurement.setFarm(farm);
             measurement.setSensorType(sensorType);
             measurementRepository.save(measurement);
         });
-        log.info("All data saved!");
+        log.info("All data saved for {}!", farmToSave.getName());
     }
 
-    private SensorTypeEntity findSensorType(FarmDataItem item) {
-        var sensorType = sensorTypeRepository.findByNameIgnoreCase(item.getSensorType().name());
+    private Map<SensorType, SensorTypeEntity> getSavedSensorTypes() {
+        Map<SensorType, SensorTypeEntity> saved = new HashMap<>();
+        Arrays.stream(SensorType.values()).forEach(sensorType -> {
+            var entity = findSensorType(sensorType.name());
+            saved.put(sensorType, entity);
+        });
+        return saved;
+    }
+
+    private SensorTypeEntity findSensorType(String name) {
+        var sensorType = sensorTypeRepository.findByNameIgnoreCase(name);
         if (sensorType.isPresent()) {
             return sensorType.get();
         }
-        return sensorTypeRepository.save(new SensorTypeEntity(item.getSensorType().name()));
+        return sensorTypeRepository.save(new SensorTypeEntity(name));
     }
 }
